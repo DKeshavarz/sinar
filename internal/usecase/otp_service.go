d61ae2b2e3c1463048d5c073c1944fc3f57d5a03
DKeshavarz <dankeshavarz1075@gmail.com>
2025-09-12 14:28:41 +0330

    add otp service

---
+++ internal/usecase/otp_service.go
@@ -0,0 +1,85 @@
+package usecase
+
+import (
+	"crypto/rand"
+	"errors"
+	"fmt"
+	"log"
+	"math/big"
+	"time"
+)
+
+type OtpStore interface {
+	Create(userID, otp string, ttl time.Duration) error
+	Get(userID string) (string, error)
+	Delete(userID string) error
+}
+
+type OtpSender interface {
+	Send(receptor, message string) error
+}
+type OtpService struct {
+	len     int
+	ttl     time.Duration
+	storage OtpStore
+	sender  OtpSender
+}
+
+func NewOtpService(len int, ttl time.Duration) *OtpService {
+
+	return &OtpService{
+		len: len,
+		ttl: ttl,
+	}
+}
+
+func (s *OtpService) RequestOTP(phone string) error {
+	code, err := generateCode(s.len)
+	if err != nil {
+		return err
+	}
+
+	if err = s.storage.Create(phone, code, s.ttl); err != nil {
+		return err
+	}
+
+	if err := s.sender.Send(phone, code); err != nil {
+		return err
+	}
+
+	log.Println("otp", code)
+
+	return nil
+}
+
+func (s *OtpService) VerifyOTP(userID, otp string) error {
+	storedOTP, err := s.storage.Get(userID)
+	if err != nil {
+		return err
+	}
+
+	if storedOTP != otp {
+		return fmt.Errorf("invalid otp")
+	}
+
+	if err := s.storage.Delete(userID); err != nil {
+		return errors.New("failed to delete OTP: " + err.Error())
+	}
+	return nil
+}
+
+// --------------- helper ------------------------
+func generateCode(length int) (string, error) {
+	const digits = "0123456789"
+	otp := make([]byte, length)
+
+	for i := range length {
+		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(digits))))
+		if err != nil {
+			return "", err
+		}
+		otp[i] = digits[n.Int64()]
+	}
+	
+	return string(otp), nil
+}
