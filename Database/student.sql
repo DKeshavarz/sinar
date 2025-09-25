7040d540b5f655f2dde224a0c0c086590f5ebdd5
DKeshavarz <dankeshavarz1075@gmail.com>
2025-09-26 00:27:13 +0330

    feat: add test data

---
+++ Database/student.sql
@@ -0,0 +1,21 @@
+-- -- Danny (already added)
+-- INSERT INTO users (first_name, last_name, phone, profile_pic, student_num, sex, university_id)
+-- VALUES ('Danny', 'Kesha', '09398113791', 'default.png', '40112358031', TRUE, 1);
+
+-- -- Sara Rahimi (Tehran University)
+-- INSERT INTO users (first_name, last_name, phone, profile_pic, student_num, sex, university_id)
+-- VALUES ('Sara', 'Rahimi', '09123456789', 'sara.png', '40222358032', FALSE, 2);
+
+-- -- Amir Hosseini (Sharif University)
+-- INSERT INTO users (first_name, last_name, phone, profile_pic, student_num, sex, university_id)
+-- VALUES ('Amir', 'Hosseini', '09381234567', 'amir.png', '40332358033', TRUE, 3);
+
+-- -- Niloofar Karimi (Isfahan University of Technology)
+-- INSERT INTO users (first_name, last_name, phone, profile_pic, student_num, sex, university_id)
+-- VALUES ('Niloofar', 'Karimi', '09217654321', 'niloofar.png', '40442358034', FALSE, 4);
+
+-- -- Hossein Ahmadi (Bu-Ali Sina again)
+-- INSERT INTO users (first_name, last_name, phone, profile_pic, student_num, sex, university_id)
+-- VALUES ('Hossein', 'Ahmadi', '09121231234', 'hossein.png', '40552358035', TRUE, 1);
+
+SELECT * FROM users;
---
+++ Database/uni.sql
@@ -0,0 +1,8 @@
+-- INSERT INTO universities (name, location, logo)
+-- VALUES
+--   ('Bu-Ali Sina University', 'Hamedan', 'buali_logo.png'),
+--   ('Tehran University', 'Tehran', 'tehran_logo.png'),
+--   ('Sharif University of Technology', 'Tehran', 'sharif_logo.png'),
+--   ('Isfahan University of Technology', 'Isfahan', 'iut_logo.png');
+
+SELECT * FROM universities;
