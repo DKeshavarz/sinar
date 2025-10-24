26de8e34e572aeb3ff70bc9da1bba5d3cdb7c094
DKeshavarz <dankeshavarz1075@gmail.com>
2025-10-24 15:03:33 +0330

    feat: change test data

--- Database/resturant.sql
+++ Database/resturant.sql
@@ -1,11 +1,13 @@
 -- For Bu-Ali Sina University (id = 1)
-INSERT INTO restaurants (university_id, name, sex, color)
-VALUES
-  (1, 'Shahrdari Restaurant', TRUE, '#1E90FF'), -- Dodger Blue
-  (1, 'Campus Cafe', FALSE, '#32CD32');        -- Lime Green
+-- INSERT INTO restaurants (university_id, name, sex, color)
+-- VALUES
+--   (1, 'Shahrdari Restaurant', TRUE, '#1E90FF'), -- Dodger Blue
+--   (1, 'Campus Cafe', FALSE, '#32CD32');        -- Lime Green
 
--- For Tehran University (id = 2)
-INSERT INTO restaurants (university_id, name, sex, color)
-VALUES
-  (2, 'Tehran Central Dining Hall', TRUE, '#FF4500'), -- Orange Red
-  (2, 'Daneshkadeh Restaurant', FALSE, '#FFD700');   -- Gold
+-- -- For Tehran University (id = 2)
+-- INSERT INTO restaurants (university_id, name, sex, color)
+-- VALUES
+--   (2, 'Tehran Central Dining Hall', TRUE, '#FF4500'), -- Orange Red
+--   (2, 'Daneshkadeh Restaurant', FALSE, '#FFD700');   -- Gold
+
+SELECT * FROM restaurants;
